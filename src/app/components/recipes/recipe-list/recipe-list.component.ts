27b752d27bac98d484d3320e91a62d19a27df2b8
Mike Langston <mike@mikelangston.me>
2018-04-06 15:58:04 -0400

    data binding and passing data between components for recipes

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output()
+  recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe("Test Recipe", "Test Description", "https://c1.staticflickr.com/9/8585/28906445485_ce32150295_b.jpg"),
-    new Recipe("Test Recipe", "Test Description", "https://c1.staticflickr.com/9/8585/28906445485_ce32150295_b.jpg")
+    new Recipe("Another Test Recipe", "Another Test Description", "https://c1.staticflickr.com/9/8585/28906445485_ce32150295_b.jpg")
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
